8690dfc01fc548942d120fa12bf14c5e6394f8f1
zhaiyujia <zhaiyujia0828@163.com>
2019-07-06 22:13:16 +0800

    feat login Test commit

--- src/main/java/com/vue/service/UserInfoService.java
+++ src/main/java/com/vue/service/UserInfoService.java
@@ -1,6 +1,5 @@
 package com.vue.service;
 
-import com.vue.dao.UserMapper;
 import com.vue.domin.UserInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,8 @@ import org.springframework.stereotype.Service;
  * @author zhaiyj
  * @date 2019/7/5 19:13
  * @instruction
- */
+ */import com.vue.dao.UserMapper;
+
 @Service
 @Slf4j
 public class UserInfoService {
